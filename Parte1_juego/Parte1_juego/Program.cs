b1039e9aee20523bce1e5b029e3fe2f0d05657e4
JUAN DANIEL GUISAO CASTRO <juanguisao246051@correo.itm.edu.co>
2024-04-05 20:19:50 -0500

    se empieza creando las variables Njugadores y para cuatro jugadores

---
+++ Parte1_juego/Parte1_juego/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("******************JUEGO ADIVINA EL NUMERO***********************");
+        Console.Write("****************************************************************");
+
+        //variables
+        double Njugadores,jugador1,jugador2,jugador3,jugador4;
+
+
+
+    }
+}
