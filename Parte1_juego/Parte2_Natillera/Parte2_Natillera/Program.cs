a759e280f4928d552426916987398ab0519a5b38
JUAN DANIEL GUISAO CASTRO <juanguisao246051@correo.itm.edu.co>
2024-04-05 21:17:37 -0500

    se crea la variable socio y total socio con un ciclo FOR para iterar para los dos socios,

---
+++ Parte1_juego/Parte2_Natillera/Parte2_Natillera/Program.cs
@@ -0,0 +1,67 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool volver = true;
+        const double BONO = 0.4; //snake case ej.(HOLA_MUNDO)
+
+        while (volver)
+        {
+            decimal aporteMensual = 0, rendimientoMensual = 0, aporteTotal = 0, rendimientoTotal = 0, bonoMensual = 0, bonoTotal = 0, aporteTotalNeto, tasaMensual;
+            ;
+
+            //class Random
+            
+            Random random = new Random();// esta es la forma de instanciar una clase en objeto
+
+            int TotalSocio;
+            int Socio =2;
+            for (int i = 1; i <= 2; i++)
+            {
+                Console.Write($"Socio {i} ingrese los siguientes datos: ");
+                for (int mes = 1; mes <= 12; mes++)
+                {
+                    Console.Write($"ingrese cantidad que desea ahorrar en el mes {mes}: ");
+                    aporteMensual = Convert.ToDecimal(Console.ReadLine());
+
+                    tasaMensual = (decimal)random.Next(1, 51) / 10;
+                    rendimientoMensual = aporteMensual * (tasaMensual / 100);
+
+                    if (tasaMensual < 1.5M)
+                    {
+                        bonoMensual = aporteMensual * (decimal)BONO;
+                        bonoTotal += bonoMensual;
+                    }
+
+                    aporteTotal += aporteMensual;
+                    rendimientoTotal += rendimientoMensual;
+
+                    Console.Write($"Mes {mes}\n" +
+                                  $"Aportes: {aporteMensual:C}\n" +
+                                  $"Tasa: {tasaMensual}% \n" +
+                                  $"Rendiemiento: {rendimientoMensual:C}\n" +
+                                  $"Bono: {bonoMensual:C}\n");
+
+                }
+
+                Console.Write("-------------------------------------------\n");
+                Console.Write("-------------------------------------------\n");
+                aporteTotalNeto = rendimientoTotal + aporteTotal + bonoTotal;
+                Console.Write($"Aportes totales: {aporteTotal:C}\n");
+                Console.Write($"Redimiento Total: {rendimientoTotal:C}\n");
+                Console.Write($"Bonos totales: {bonoTotal:C} \n");
+                Console.Write("-------------------------------------------\n");
+                Console.Write("-------------------------------------------\n");
+                Console.Write($"Total neto: {aporteTotalNeto:C}\n");
+
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+                Console.WriteLine("Desea continuar en la natillera? (s/n)");
+                string continuar = Console.ReadLine().ToLower();
+
+                if (continuar == "n") volver = false;
+            }
+
+        }
+    }
+}
